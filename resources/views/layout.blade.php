6f43b82407644806218d33fafaf06d40cc9f0d73
Alex Dunia <thealexdunia@gmail.com>
2023-02-22 15:33:41 +0100

    updates

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        {{-- @stack('css') --}}
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="images/favicon.ico" />
+      
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;
+        400;500;600;700;800;900&family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;
+        0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:
+        ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;
+        1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        
+        {{-- <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <script src="https://cdn.tailwindcss.com"></script> --}}
+   
+        </script>
+        
+        <title> Friendly blocks </title>
+    </head>
+    <body>
+
+    <div class="navcolor">
+
+        <div class="navflex"> 
+
+        <div> 
+        <img src="\images\fbs.png" width="230px"/>
+        </div> 
+
+        <div class="navdiv"> 
+            <ul> 
+                <a href="">  <li> Home </li>  </a> 
+                <a href="">  <li> About </li>  </a> 
+                <a href="">  <li> Contact us </li>  </a> 
+                <a href="">  <li> Blog </li>  </a> 
+            </ul> 
+         </div> 
+
+
+         <div class="navdivregister"> 
+            <ul> 
+                <a href="">  <li> Login </li>  </a> 
+                <a href="">  <li> Sign up </li>  </a> 
+            </ul> 
+         </div> 
+
+        </div>
+
+    </div>
+
+    {{-- Hero div --}}
+
+    <div class="heroflex"> 
+
+    <div class="heromaintext"> 
+        <h2> Web3 stuffs, <br/> Simplified </h2>
+        <p> Have advanced blockchain concepts  <br/>
+            explained to you in 
+            the simplest ways <br/> 
+        </p>
+
+        <button class="wlbtnn" type="submit">  Join the wating list </button>
+        
+    </div>
+
+    <div> 
+        <img src="\images\on_chain.png" width="500px"/>
+    </div>
+
+</div>
+
+
+        @yield('mycontent')
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout')
+@section('mycontent')
+
+
+
+<link rel="stylesheet" href="css/style.css">
+{{-- @push('css')
+    <link rel="stylesheet" href="css/main.css">
+@endpush --}}
+
+<h1 class="omo"> Hello fucking world </h1>
+
+@endsection
