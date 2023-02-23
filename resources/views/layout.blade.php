73001869798c8fafa67003d0a3e542858b4c070e
Alex Dunia <thealexdunia@gmail.com>
2023-02-23 12:29:47 +0100

    updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -70,8 +70,8 @@
             the simplest ways <br/> 
         </p>
 
-        <button class="wlbtnn" type="submit">  Join the wating list </button>
-        
+        <button class="wlbtnn" type="submit">  Get started </button>
+
     </div>
 
     <div> 
@@ -79,6 +79,67 @@
     </div>
 
 </div>
+
+ {{-- Next div --}}
+
+ <div class="everyoneflex"> 
+
+    <div class="everyoneimg"> 
+        <img src="\images\ref.png" width="410px"/>
+    </div>
+
+    <div class="everyonemaintext"> 
+        <h2> Blockchain Technology is  for everyone </h2>
+        <p> Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an 
+
+            <br/> 
+            <br/>
+            
+            Unknown printer took a galley of type and scrambled it to
+             make a type specimen book. It has survived not only five
+             centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged.
+        </p>
+
+        {{-- <button class="wlbtnn" type="submit">  Join the wating list </button> --}}
+
+    </div>
+
+
+</div>
+
+<div class="appbg"> 
+
+    <div class="mainappflex">
+
+    <div class="mainapp"> 
+        <h1> Our approach </h1> 
+        <br/>
+        <br/> 
+        <h3> Lorem Ipsum </h3> 
+        <p> Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry. Lorem Ipsum has been the industry's  
+            standard dummy text ever since the 1500s. </p>
+
+            <br/>
+            <br/> 
+            <h3> Lorem Ipsum </h3> 
+            <p> Lorem Ipsum is simply dummy text of the printing and 
+                typesetting industry. Lorem Ipsum has been the industry's  
+                standard dummy text ever since the 1500s. </p>
+    </div> 
+
+
+    <div class="mainappimg"> 
+        <img src="\images\crypto.png" width="450px"/>
+    </div> 
+
+    </div>   
+
+</div> 
+
 
 
         @yield('mycontent')
