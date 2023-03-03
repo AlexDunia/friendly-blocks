36ac5e07aa92d19a358503d19fe85486a371a19c
Alex Dunia <thealexdunia@gmail.com>
2023-03-03 21:36:40 +0100

    updates

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,54 @@
 @extends('layout')
 @section('mycontent')
 
-<h1> Hello </h1>
+<head>
+
+    <link
+        rel="stylesheet"
+        href="\css\profile.css" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/solid.css">
+        <script src="https://kit.fontawesome.com/9ff47ec0f8.js" crossorigin="anonymous"> </script>
+
+</head>
+
+<div class="loginuserphoto"> 
+    <img src="\images\dunia.png" class="loginuserphotoimage"> 
+    <h1> Alex Dunia </h1>
+</div>
+
+<div class="reachlinksdiv">  
+
+<div class="reachlinks"> 
+    <div class="reachimg"> 
+        <img src="\images\github.png"> 
+    </div> 
+
+    <div class="reachtext"> 
+        <h3> Github </h3> 
+    </div> 
+</div> 
+
+<div class="reachlinks"> 
+    <div class="reachimg"> 
+        <img src="\images\twitter.png"> 
+    </div> 
+
+    <div class="reachtext"> 
+        <h3> Twitter </h3> 
+    </div> 
+</div> 
+
+
+<div class="reachlinks"> 
+    <div class="reachimg"> 
+        <img src="\images\linkedin.png"> 
+    </div> 
+
+    <div class="reachtext"> 
+        <h3> Linkedin </h3> 
+    </div> 
+</div> 
+
+</div> 
 
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,12 +6,6 @@
     <link
         rel="stylesheet"
         href="\css\register.css" />
-        {{-- <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer" --}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/solid.css">
         <script src="https://kit.fontawesome.com/9ff47ec0f8.js" crossorigin="anonymous"> </script>
 
